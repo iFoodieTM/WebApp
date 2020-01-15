fabc08ecb9727eb92f9670f37de2058bb66716a0
Ricardo Morales <ricardomoralesfer@yahoo.com>
2020-01-15 14:28:46 +0100

    bootstrap, login

--- WebApp.js
+++ WebApp.js
@@ -1,19 +1,26 @@
 window.onload = cargar;
 
     function cargar(){
-        document.getElementById("btn").onclick = register_user;
+      document.getElementById("btn btn-default").onclick = register_user;
     } 
 
     function register_user(){
-        var URL = "http://localhost:8888/Victor/APILibrary-master/public/index.php/api/storeUser";
-        var email = document.getElementById("email").value;
-        var password = document.getElementById("password").value;
+        var URL = "http://localhost:8888/Rick/Password/public/index.php/api/login";
+        var email = document.getElementById("nombre").value;
+        var password = document.getElementById("clave").value;
         var user_data = {'email':email, 'password':password};
-
-     $.ajax(URL,{
-        type: "POST",
+console.log(user_data);
+     $.ajax({
+        url: URL,
+        type: 'POST',
         data: user_data,
+        datatype: 'json',
+        success: function(response){
+          alert(response.token);
+          sessionStorage.setItem('token', response.token);
+      }
       }).done(function(){
         alert("información correcta")
+        console.log("5")
       });
     }
